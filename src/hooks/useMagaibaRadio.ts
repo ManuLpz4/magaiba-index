eb273594a20a714df34f4f3818d0c0f367a73ed5
LautaroLopez01 <lauchalopez4@gmail.com>
2024-03-12 13:42:28 -0300

    Eliminado los comentarios

--- src/hooks/useMagaibaRadio.ts
+++ src/hooks/useMagaibaRadio.ts
@@ -4,7 +4,7 @@ import { useEffect, useState, useRef } from "react";
 export const useMagaibaRadio = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [volume, setVolume] = useState(0.5);
-  const audioRef = useRef(new Audio("/music/magaiba.mp3")); // Referencia al reproductor de audio
+  const audioRef = useRef(new Audio("/music/magaiba.mp3"));
 
   const toggleMagaibaRadio = () => {
     setIsPlaying(!isPlaying);
@@ -13,14 +13,14 @@ export const useMagaibaRadio = () => {
   const increaseVolume = () => {
     if (volume < 1) {
       setVolume(Math.min(volume + 0.1, 1));
-      audioRef.current.volume = Math.min(volume + 0.1, 1); // Actualizar el volumen del audio
+      audioRef.current.volume = Math.min(volume + 0.1, 1); 
     }
   };
 
   const decreaseVolume = () => {
     if (volume > 0) {
       setVolume(Math.max(volume - 0.1, 0));
-      audioRef.current.volume = Math.max(volume - 0.1, 0); // Actualizar el volumen del audio
+      audioRef.current.volume = Math.max(volume - 0.1, 0);
     }
   };
 
